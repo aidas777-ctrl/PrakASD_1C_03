be22c1b0cfc950573c55e51a8dfa141241c5ba94
aidas777-ctrl <fadhila2903@gmail.com>
2024-02-29 11:10:59 +0700

    P2_latihan1_1C_03

--- P2/PenjualanBuku/Buku03.java
+++ P2/PenjualanBuku/Buku03.java
@@ -4,31 +4,30 @@ public class Buku03 {
     String judul, pengarang;
     int halaman, stok, harga;
 
-    public Buku03(){
-
+    public Buku03() {
+        
     }
-    
-    public Buku03 (String jud, String pg, int hal, int stok, int har){
+
+    public Buku03(String jud, String pg, int hal, int stok, int har) {
         judul = jud;
         pengarang = pg;
         halaman = hal;
         this.stok = stok;
         harga = har;
-
-
     }
+
     void tampilInformasi() {
-        System.out.println("Judul: " + judul);
-        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Judul         : " + judul);
+        System.out.println("Pengarang     : " + pengarang);
         System.out.println("Jumlah Halaman: " + halaman);
-        System.out.println("Sisa Stok: " + stok);
-        System.out.println("Harga: Rp " + harga);
+        System.out.println("Sisa Stok     : " + stok);
+        System.out.println("Harga/Buku    : Rp " + harga);
     }
 
     void terjual(int jml) {
         if (stok > 0 && stok >= jml) {
             stok -= jml;
-        }else{
+        } else {
             System.out.println("Stok Habis");
         }
     }
@@ -40,4 +39,42 @@ public class Buku03 {
     void gantiHarga(int hrg) {
         harga = hrg;
     }
+
+    void hitungHargaTotal(int terJual) {
+        int hargaTotal = harga * terJual;
+        System.out.println("Harga Total   : Rp " + hargaTotal);
+    }
+
+    void hitungDiskon(int terJual) {
+        int hargaTotal = harga * terJual;
+        double diskon = 0;
+        double jumlahDis = 0;
+
+        if (hargaTotal > 150000) {
+            jumlahDis = 0.12 * hargaTotal;
+        } else if (hargaTotal > 75000 && hargaTotal < 150000) {
+            jumlahDis = 0.05 * hargaTotal;
+        } else if (hargaTotal < 75000) {
+            diskon = 0;
+        }
+        System.out.println("Diskon        : Rp " + jumlahDis);
+    }
+
+    void hitungHargaBayar(int terJual) {
+        int hargaTotal = harga * terJual;
+        double diskon = 0;
+        double jumlahDis = 0;
+
+        if (hargaTotal > 150000) {
+            jumlahDis = 0.12 * hargaTotal;
+        } else if (hargaTotal > 75000 && hargaTotal < 150000) {
+            jumlahDis = 0.05 * hargaTotal;
+        } else if (hargaTotal < 75000) {
+            diskon = 0;
+        }
+
+        double hargaBayar  = hargaTotal - jumlahDis;
+        System.out.println("Harga bayar   : Rp " + hargaBayar);
+    }
+
 }
--- P2/PenjualanBuku/BukuMain03.java
+++ P2/PenjualanBuku/BukuMain03.java
@@ -3,26 +3,36 @@ package P2.PenjualanBuku;
 public class BukuMain03 {
     public static void main(String[] args) {
         Buku03 bk1 = new Buku03();
-        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.judul     = "Today Ends Tomorrow Comes";
         bk1.pengarang = "Denanda Pratiwi";
-        bk1.halaman = 198;
-        bk1.stok = 13;
-        bk1.harga = 71000;
-
-        bk1.tampilInformasi();
+        bk1.halaman   = 198;
+        bk1.stok      = 13;
+        bk1.harga     = 71000;
+        System.out.println();
         bk1.terjual(5);
+        bk1.tampilInformasi();
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+        System.out.println();
 
 
-        Buku03 bk2 = new Buku03("Self Reward", "Maheera Ayesha", 160, 29, 59000);
-        bk2.terjual(11);
+        Buku03 bk2 = new Buku03("Self Reward", "Maheera Ayesha", 160, 29, 60000 );
+        bk2.terjual(10);
         bk2.tampilInformasi();
+        // bk2.hitungHargaTotal(10);
+        // bk2.hitungDiskon(10);
+        // bk2.hitungHargaBayar(10);
+        System.out.println();
+       
+       
 
-
-        Buku03 bukuAida = new Buku03("Antara Aku dan Dia", "Aida Rahma", 200, 99, 50000);
-        bukuAida.terjual (98);
+        Buku03 bukuAida = new Buku03("Antara Aku dan Dia", "Aida Rahma", 200, 99, 20000 );
+        bukuAida.terjual (4);
         bukuAida.tampilInformasi();
+        bukuAida.hitungHargaTotal(4);
+        bukuAida.hitungDiskon(4);
+        bukuAida.hitungHargaBayar(4);
+        System.out.println();
 
         
     }
