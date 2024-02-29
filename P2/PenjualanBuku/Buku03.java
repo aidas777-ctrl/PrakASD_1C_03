9f66248265acc055141a2094fd7d06e8992b1458
aidas777-ctrl <fadhila2903@gmail.com>
2024-02-29 07:39:47 +0700

    P2_percobaan1_1C_03

---
+++ P2/PenjualanBuku/Buku03.java
@@ -0,0 +1,26 @@
+package P2.PenjualanBuku;
+
+public class Buku03 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah Halaman: " + halaman);
+        System.out.println("Sisa Stok: " + stok);
+        System.out.println("Harga: " + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
