73c91d394b4728fc9607482dd018cab6890486c0
aidas777-ctrl <fadhila2903@gmail.com>
2024-03-07 09:25:54 +0700

    P3_percobaan3_pertanyaan1_1C_03

--- P3/ArrayBalok/Balok03.java
+++ P3/ArrayBalok/Balok03.java
@@ -5,6 +5,12 @@ public class Balok03 {
     public int lebar;
     public int tinggi;
 
+    public Balok03(){
+       panjang = 0;
+       lebar = 0;
+       tinggi = 0;
+    }
+
     public Balok03 (int p, int l, int t){
         panjang = p;
         lebar = l;
