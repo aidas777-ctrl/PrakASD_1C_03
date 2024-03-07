7516bd9934742d34e04018e00d2c8e7e8edc07aa
aidas777-ctrl <fadhila2903@gmail.com>
2024-03-07 08:53:28 +0700

    P3_percobaan2_pertanyaan2_1C_03

--- P3/AOBPersegiPanjang/ArrayObjects03.java
+++ P3/AOBPersegiPanjang/ArrayObjects03.java
@@ -25,5 +25,27 @@ public class ArrayObjects03 {
             System.out.println("Panjang: " + ppArray[i].panjang + ", lebar: " + ppArray[i].lebar);
 
         }
+
+
+       PersegiPanjang03 [][] ppArray2 = new PersegiPanjang03[3][2];
+       for (int i = 0; i < ppArray2.length; i++) {
+        for (int j = 0; j < ppArray2[i].length; j++) {
+            ppArray2[i][j] = new PersegiPanjang03();
+            System.out.println("Persegi Panjang ke-" + i + ", " + j);
+
+            System.out.print("Masukkan Panjang: ");
+            ppArray2[i][j].panjang = aida03.nextInt();
+
+            System.out.print("Masukkan Lebar: ");
+            ppArray2[i][j].lebar = aida03.nextInt();
+        }
+    }
+
+    for (int i = 0; i < ppArray2.length; i++) {
+        for (int j = 0; j < ppArray2[i].length; j++) {
+            System.out.println("Persegi Panjang ke-" + i + ", " + j);
+            System.out.println("Panjang: " + ppArray2[i][j].panjang + ", lebar: " + ppArray2[i][j].lebar);
+        }
+    }
     }
 }
