fec9602d2891735a4bf73fd0c918f6762b750e58
aidas777-ctrl <fadhila2903@gmail.com>
2024-03-07 09:39:11 +0700

    P3_percobaan3_pertanyaan2_1C_03

---
+++ P3/ArrayBalok/Segitiga03.java
@@ -0,0 +1,11 @@
+package P3.ArrayBalok;
+
+public class Segitiga03 {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga03(int a, int t){
+        alas = a;
+        tinggi = t;
+    }
+}
