4e5fdacede5c833830c34973caf7faf2077ff3b0
aidas777-ctrl <fadhila2903@gmail.com>
2024-03-07 08:58:56 +0700

    P3_percobaan2_pertanyaan3_1C-03

---
+++ P3/AOBPersegiPanjang/Persegi03.java
@@ -0,0 +1,11 @@
+package P3.AOBPersegiPanjang;
+
+public class Persegi03 {
+    public int sisi;
+
+    public static void main(String[] args) {
+        Persegi03[] pgArray = new Persegi03[100];
+        pgArray[5].sisi = 20;
+
+    }
+}
