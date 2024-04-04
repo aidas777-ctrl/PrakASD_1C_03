bfaa2df51ddd15c83ed3f2a6df18a9df2a6c632f
aidas777-ctrl <fadhila2903@gmail.com>
2024-04-04 09:55:02 +0700

    P7_percobaan2_1C_03

--- P7/BukuMain03.java
+++ P7/BukuMain03.java
@@ -8,12 +8,13 @@ public class BukuMain03 {
         Scanner s1 = new Scanner(System.in);
 
         PencarianBuku03 data = new PencarianBuku03();
+        int jumBuku = 5;
 
-        Buku03 m = new Buku03(127, "Algoritma", 2019, "Wahyuni", 5);
+        Buku03 m = new Buku03(111, "Algoritma", 2019, "Wahyuni", 5);
         Buku03 m1 = new Buku03(123, "Big Data", 2020, "Susilo", 3);
         Buku03 m2 = new Buku03(125, "Desain UI", 2021, "Supriadi", 3);
-        Buku03 m3 = new Buku03(111, "Web Programing", 2022, "Pustaka Adi", 2);
-        Buku03 m4 = new Buku03(126, "Etika Mahasiswa", 2023, "Darmawan Adi", 2);
+        Buku03 m3 = new Buku03(126, "Web Programing", 2022, "Pustaka Adi", 2);
+        Buku03 m4 = new Buku03(127, "Etika Mahasiswa", 2023, "Darmawan Adi", 2);
 
         data.tambah(m);
         data.tambah(m1);
@@ -26,23 +27,30 @@ public class BukuMain03 {
         System.out.println("Data keseluruhan Buku");
         data.tampil();
 
-        System.out.println("_____________________________________________");
-        System.out.println("_____________________________________________");
+        System.out.println("________________________________________");
+        System.out.println("________________________________________");
         System.out.println("Pencarian Data : ");
         System.out.println("Masukkan Kode Buku yang dicari: ");
         System.out.print("Kode Buku : ");
         int cari = s.nextInt();
+
         System.out.println("menggunakan sequential Search");
         int posisi = data.FindSeqSearch(cari);
         data.tampilPoisisi(cari, posisi);
         data.TampilData(cari, posisi);
 
-        Buku03 dataBuku = data.FindBuku(cari);
-        if (dataBuku != null) {
-            System.out.println("Data Buku yang ditemukan:");
-            dataBuku.tampilDataBuku();
-        } else {
-            System.out.println("Data Buku tidak ditemukan.");
-        }
+        // Buku03 dataBuku = data.FindBuku(cari);
+        // if (dataBuku != null) {
+        //     System.out.println("Data Buku yang ditemukan:");
+        //     dataBuku.tampilDataBuku();
+        // } else {
+        //     System.out.println("Data Buku tidak ditemukan.");
+        // }
+
+        System.out.println("================================");
+        System.out.println("menggunakan binary search");
+        posisi = data.FindBinarySearch(cari, 0, jumBuku -1);
+        data.tampilPoisisi(cari, posisi);
+        data.TampilData(cari, posisi);
     }
 }
