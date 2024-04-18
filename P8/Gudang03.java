87b37dff04d05f4e4cc49cea2f88e805ef9feb39
aidas777-ctrl <fadhila2903@gmail.com>
2024-04-18 07:49:55 +0700

    P8_percobaan1_pertanyaan3_1C_03

--- P8/Gudang03.java
+++ P8/Gudang03.java
@@ -61,14 +61,14 @@ public class Gudang03 {
     }
 
     public void tampilkanBarang() {
-        if (!cekKosong()) {
+        // if (!cekKosong()) {
             System.out.println("Rincian tumpukan barang di Gudang:");
             for (int i = top; i >= 0; i--) {
                 System.out.printf("Kode %d: %s (Kategori %s)\n", tumpukan[i].kode, tumpukan[i].nama,
                         tumpukan[i].kategori);
             }
-        } else {
-            System.out.println("Tumpukan barang kosong.");
-        }
+        // } else {
+        //     System.out.println("Tumpukan barang kosong.");
+        // }
     }
 }
