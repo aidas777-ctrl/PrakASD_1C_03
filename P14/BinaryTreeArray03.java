79adc85eefd5808ef52b2172584b62ab2037195a
aidas777-ctrl <fadhila2903@gmail.com>
2024-05-30 12:08:45 +0700

    P14_Tugas5_1C_03

--- P14/BinaryTreeArray03.java
+++ P14/BinaryTreeArray03.java
@@ -8,6 +8,28 @@ public class BinaryTreeArray03 {
         data = new int[10];
     }
 
+    public void add(int data) {
+        if (idxLast == -1) {
+            this.data[0] = data;
+            idxLast++;
+        } else {
+            int currentIndex = idxLast + 1;
+            this.data[currentIndex] = data;
+            idxLast++;
+            while (currentIndex > 0) {
+                int parentIndex = (currentIndex - 1) / 2;
+                if (this.data[currentIndex] < this.data[parentIndex]) {
+                    int temp = this.data[currentIndex];
+                    this.data[currentIndex] = this.data[parentIndex];
+                    this.data[parentIndex] = temp;
+                    currentIndex = parentIndex;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
     void populateData(int data[], int idxLast) {
         this.data = data;
         this.idxLast = idxLast;
@@ -18,6 +40,22 @@ public class BinaryTreeArray03 {
             traverseInOrder(2 * idxStart + 1);
             System.out.print(data[idxStart] + " ");
             traverseInOrder(2 * idxStart + 2);
+        }
+    }
+
+    void traversePreOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            System.out.print(data[idxStart] + " ");
+            traversePreOrder(2 * idxStart + 1);
+            traversePreOrder(2 * idxStart + 2);
+        }
+    }
+
+    void traversePostOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            traversePostOrder(2 * idxStart + 1);
+            traversePostOrder(2 * idxStart + 2);
+            System.out.print(data[idxStart] + " ");
         }
     }
 }
--- P14/BinaryTreeArrayMain03.java
+++ P14/BinaryTreeArrayMain03.java
@@ -6,10 +6,19 @@ public class BinaryTreeArrayMain03 {
         int[] data = { 6, 4, 8, 3, 5, 7, 9, 0, 0, 0 };
         int idxLast = 6;
 
+        for (int i = 0; i <= idxLast; i++) {
+            bta.add(data[i]);
+        }
+
         bta.populateData(data, idxLast);
         System.out.print("\nInOrder Traversal : ");
         bta.traverseInOrder(0);
-        System.out.println("\n");
+
+        System.out.print("\nPreOrder Traversal : ");
+        bta.traversePreOrder(0);
+
+        System.out.print("\nPostOrder Traversal : ");
+        bta.traversePostOrder(0);
     }
 
 }
