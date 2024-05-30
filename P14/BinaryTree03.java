8f74b31f24e9af7871345d15c292ba5d6f599ed5
aidas777-ctrl <fadhila2903@gmail.com>
2024-05-30 10:53:50 +0700

    P14_percobaan1_1C_03

---
+++ P14/BinaryTree03.java
@@ -0,0 +1,161 @@
+package P14;
+
+public class BinaryTree03 {
+    Node03 root;
+
+    public BinaryTree03() {
+        root = null;
+    }
+
+    boolean isEmpty() {
+        return root == null;
+    }
+
+    void add(int data) {
+        if (isEmpty()) { // tree is empty
+            root = new Node03(data);
+        } else {
+            Node03 current = root;
+            while (true) {
+                if (data < current.data) {
+                    if (current.left != null) {
+                        current = current.left;
+                    } else {
+                        current.left = new Node03(data); 
+                        break;
+                    }
+                } else if (data > current.data) {
+                    if (current.right == null) {
+                        current.right = new Node03(data);
+                        break;
+                    } else {
+                        current = current.right;
+                    }
+                } else {
+                    System.out.println("Data sudah ada");
+                    break;
+                }
+            }
+        }
+    }
+
+    boolean find(int data) {
+        Node03 current = root;
+        while (current != null) {
+            if (current.data == data) {
+                return true;
+            } else if (data < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false;
+    }
+
+    void traversePreOrder(Node03 node) {
+        if (node != null) {
+            System.out.print(" " + node.data);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    void traversePostOrder(Node03 node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.data);
+        }
+    }
+
+    void traverseInOrder(Node03 node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.data);
+            traverseInOrder(node.right);
+        }
+    }
+
+    Node03 getSuccessor(Node03 del) {
+        Node03 successor = del.right;
+        Node03 successorParent = del;
+        while (successor.left != null) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+    }
+
+    void delete(int data) {
+        if (isEmpty()) {
+            System.out.println("Tree is empty!");
+            return;
+        }
+
+        Node03 parent = root;
+        Node03 current = root;
+        boolean isLeftChild = false;
+
+        while (current != null && current.data != data) {
+            parent = current;
+            if (data < current.data) {
+                current = current.left;
+                isLeftChild = true;
+            } else {
+                current = current.right;
+                isLeftChild = false;
+            }
+        }
+
+        if (current == null) {
+            System.out.println("Couldn't find data!");
+            return;
+        }
+        if (current.left == null && current.right == null) {
+            if (current == root) {
+                root = null;
+            } else if (isLeftChild) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        }
+
+        else if (current.left == null) {
+            if (current == root) {
+                root = current.right;
+            } else if (isLeftChild) {
+                parent.left = current.right;
+            } else {
+                parent.right = current.right;
+            }
+        }
+
+        else if (current.right == null) {
+            if (current == root) {
+                root = current.left;
+            } else if (isLeftChild) {
+                parent.left = current.left;
+            } else {
+                parent.right = current.left;
+            }
+        }
+
+        else {
+            Node03 successor = getSuccessor(current);
+            if (current == root) {
+                root = successor;
+            } else if (isLeftChild) {
+                parent.left = successor;
+            } else {
+                parent.right = successor;
+            }
+            successor.left = current.left;
+        }
+    }
+}
