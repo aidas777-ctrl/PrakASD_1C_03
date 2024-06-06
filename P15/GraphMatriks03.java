b3dd120e041099a5ac0e01154a9543464ff31e05
aidas777-ctrl <fadhila2903@gmail.com>
2024-06-06 11:52:24 +0700

    P15_percobaan2_pertanyaan1_1C_03

--- P15/GraphMatriks03.java
+++ P15/GraphMatriks03.java
@@ -14,7 +14,7 @@ public class GraphMatriks03 {
     }
 
     public void removeEdge(int asal, int tujuan){
-        matriks[asal][tujuan] = -1 ;
+        matriks[asal][tujuan] = 0 ;
     }
 
     public void printGraph(){
