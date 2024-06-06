a9f825b8ddb08fe40694b01ca6a00634f0ba5577
aidas777-ctrl <fadhila2903@gmail.com>
2024-06-06 17:31:00 +0700

    P15_percobaan2_pertanyaan4_1C_03_revisi_pindahPackageSendiri

---
+++ P15/GraphMatriks/GraphMatriks03.java
@@ -0,0 +1,59 @@
+package P15.GraphMatriks;
+
+public class GraphMatriks03 {
+    int vertex;
+    int[][] matriks;
+
+    public GraphMatriks03(int v){
+        vertex = v;
+        matriks = new int[v][v];
+    }
+
+    public void makeEdge(int asal, int tujuan, int jarak){
+        matriks[asal][tujuan] = jarak;
+    }
+
+    public void removeEdge(int asal, int tujuan){
+        matriks[asal][tujuan] = 0 ;
+    }
+
+    public void printGraph(){
+        for (int i = 0; i < vertex; i++) {
+            System.out.print("Gedung " + (char) ('A' + i) + ": ");
+            for (int j = 0; j < vertex; j++) {
+                if (matriks[i][j] != -1) {
+                    System.out.print("Gedung " + (char) ('A' + j) + "(" + matriks[i][j] +  " m), ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public int outDegree(int v) {
+        int outDegree = 0;
+        for (int i = 0; i < vertex; i++) {
+            if (matriks[v][i] != 0) {
+                outDegree++;
+            }
+        }
+        return outDegree;
+    }
+
+    public int inDegree(int v) {
+        int inDegree = 0;
+        for (int i = 0; i < vertex; i++) {
+            if (matriks[i][v] != 0) {
+                inDegree++;
+            }
+        }
+        return inDegree;
+    }
+
+    public void printDegree() {
+        for (int i = 0; i < vertex; i++) {
+            int inDegree = inDegree(i);
+            int outDegree = outDegree(i);
+            System.out.println("Gedung " + (char) ('A' + i) + " - inDegree: " + inDegree + ", outDegree: " + outDegree);
+        }
+    }
+}
